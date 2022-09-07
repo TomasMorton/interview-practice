8288abd8d61183caf5b7921e3b0865a20540d75b
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-09-07 08:37:16 -0400

    Print all subsequences of a string

---
+++ InterviewPractice.Solutions/AllSubranges.cs
@@ -0,0 +1,15 @@
+﻿namespace InterviewPractice.Solutions;
+
+public class AllSubranges
+{
+    public static List<string> Find(string s)
+    {
+        if (s.Length == 0)
+            return new List<string>();
+
+        var result = Find(s[1..]);
+        var nextCharacter = s[0];
+        var withNextCharacter = result.Select(x => nextCharacter + x);
+        return new[] { nextCharacter.ToString() }.Concat(withNextCharacter).Concat(result).ToList();
+    }
+}
