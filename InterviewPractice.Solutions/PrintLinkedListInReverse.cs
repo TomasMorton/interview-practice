bd68f91dbfb5470b94eec69ebe1f2aa90582ac27
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-09-07 07:54:09 -0400

    Print a linked list in reverse order

---
+++ InterviewPractice.Solutions/PrintLinkedListInReverse.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewPractice.Solutions;
+
+public class PrintLinkedListInReverse<T>
+{
+    public static void Print(LinkedList<T>? list)
+    {
+        if (list == null)
+            return;
+
+        Console.Write("[");
+        Print(list.First);
+        Console.WriteLine("]");
+    }
+
+    private static void Print(LinkedListNode<T>? head)
+    {
+        if (head == null)
+            return;
+
+        Print(head.Next);
+        Console.Write($" {head.Value};");
+    }
+}
