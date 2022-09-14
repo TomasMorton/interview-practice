7091e4b318cc223a693c10095e49dab2f054b78a
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-09-14 08:51:15 -0400

    Count the number of paths up a set of stairs

---
+++ InterviewPractice.Solutions/StairSteps.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewPractice.Solutions;
+
+public class StairSteps
+{
+    public int GetUniquePaths(int numberOfSteps)
+    {
+        if (numberOfSteps <= 2)
+            return numberOfSteps;
+
+        return GetUniquePaths(numberOfSteps - 1) + GetUniquePaths(numberOfSteps - 2);
+    }
+
+    public int GetUniquePathsIterative(int numberOfSteps)
+    {
+        if (numberOfSteps <= 2)
+            return numberOfSteps;
+
+        var last = 2;
+        var secondLast = 1;
+
+        for (var step = 3; step <= numberOfSteps; step++)
+        {
+            var currentTotal = last + secondLast;
+            secondLast = last;
+            last = currentTotal;
+        }
+
+        return last;
+    }
+}
