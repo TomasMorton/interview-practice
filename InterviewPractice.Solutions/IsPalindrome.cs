48ef7ff4b9f95693ef606d9d18b5fea500bc5a2a
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-09-14 08:37:12 -0400

    Check if string is a palindrome

---
+++ InterviewPractice.Solutions/IsPalindrome.cs
@@ -0,0 +1,15 @@
+﻿namespace InterviewPractice.Solutions;
+
+public class IsPalindrome
+{
+    public bool CheckString(string s)
+    {
+        if (s.Length <= 1)
+            return true;
+
+        if (s[0] != s[^1])
+            return false;
+
+        return CheckString(s[1..^1]);
+    }
+}
