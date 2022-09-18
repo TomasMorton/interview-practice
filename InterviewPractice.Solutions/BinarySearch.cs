0247906636d4d6641a579d8492f86ff2b77f3594
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-09-18 08:43:01 -0400

    Binary search

---
+++ InterviewPractice.Solutions/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace InterviewPractice.Solutions;
+
+public class BinarySearch
+{
+    public static void Test()
+    {
+        var arr = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var result = new BinarySearch().Find(arr, 1);
+        Console.WriteLine(result);
+    }
+    
+    public int? Find(int[] data, int target)
+    {
+        return Find(new ArraySegment<int>(data), target);
+    }
+
+    private int? Find(ArraySegment<int> data, int target)
+    {
+        if (data.Count == 0)
+            return null;
+
+        var midPoint = data.Count / 2;
+        if (data[midPoint] == target)
+            return midPoint + data.Offset;
+        
+        if (data[midPoint] > target)
+            return Find(data[..(midPoint)], target);
+
+        return Find(data[(midPoint + 1)..], target);
+    }
+}
